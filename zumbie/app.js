97f0ee8a56bbd344e0e37203f4fe8370e5c875c2
Thiago Capelo <thiago@kpelo.com.br>
2016-03-10 18:04:54 -0300

    Validação no Controller

--- zumbie/app.js
+++ zumbie/app.js
@@ -4,8 +4,12 @@ angular.module('app').controller('zumbisCtrl', function($scope) {
 
 	$scope.ocorrencias = [];
 
-	$scope.adiciona = function(ocorrencia) {
-		$scope.ocorrencias.push(angular.copy(ocorrencia));
+	$scope.adiciona = function(ocorrencia, formIsValid) {
+		if(formIsValid){
+			$scope.ocorrencias.push(angular.copy(ocorrencia));
+		} else {
+			alert('form invalido');
+		}
 	}
 });
 
