befa0162f06d8fccc1fc92a93989d2a528a7be24
Thiago Capelo <thiago@kpelo.com.br>
2016-03-10 16:39:04 -0300

    Create AppZumbie

---
+++ zumbie/app.js
@@ -0,0 +1,10 @@
+angular.module('app', []);
+
+angular.module('app').controller('zumbisCtrl', function($scope) {
+
+	$scope.ocorrencias = [];
+
+	$scope.adiciona = function(ocorrencia) {
+		$scope.ocorrencias.push(angular.copy(ocorrencia));
+	}
+});
